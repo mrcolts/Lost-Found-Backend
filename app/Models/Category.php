e0cc06907d8629e7cfc950fda577f252fcfb193e
alexey <demchenko.alexey.s@gmail.com>
2020-11-10 02:44:02 +0600

    Previous commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Category
+ *
+ * @mixin Builder
+ */
 class Category extends Model
 {
+
     protected $fillable = [
       'name'
     ];

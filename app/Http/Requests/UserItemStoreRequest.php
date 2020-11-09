8c8dd5215f141a12ecf6305e0fb89bd8071c1ae9
alexey <demchenko.alexey.s@gmail.com>
2020-11-10 01:08:25 +0600

    Requests

---
+++ app/Http/Requests/UserItemStoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserItemStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'email'],
+            'avatar' => ['required', 'image', 'max:8192'],
+            'category' => ['required', 'uuid', 'exists:categories,id']
+        ];
+    }
+}

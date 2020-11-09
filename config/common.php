b4c27e12643c7a62e7e13666f71312b440adc5d6
MrColT <coltredstone@gmail.com>
2020-11-10 02:47:34 +0600

    1

--- config/common.php
+++ config/common.php
@@ -2,7 +2,6 @@
 
 return [
     'web' => [
-        'lost_item_page' => env('WEB_PAGE', 'https://lostandfound.movily.app') + env('WEB_LOST_ITEM_PAGE')
+        'lost_item_page' => env('WEB_PAGE', 'https://lostandfound.movily.app') . env('WEB_LOST_ITEM_PAGE')
     ]
-
 ];

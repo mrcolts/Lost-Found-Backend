b52dabc5f0427c3624b5420d8cf270ae499076a2
alexey <demchenko.alexey.s@gmail.com>
2020-11-10 00:35:30 +0600

    User model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('fullName');
+            $table->string('full_name');
             $table->string('password');
             $table->string('img_index')->nullable(true);
 

28dbb29736b202fb756bf71cf8062e8ee18c420f
alexey <demchenko.alexey.s@gmail.com>
2020-11-10 06:57:54 +0600

    Sleep

--- app/Http/Resources/MeItemsResource.php
+++ app/Http/Resources/MeItemsResource.php
@@ -19,7 +19,7 @@ class MeItemsResource extends JsonResource
             'name' => $this['name'],
             'imageIndex' => $this['img_index'],
             'description' => $this['description'],
-            'categories' => CategoriesResource::collection($this['categories']),
+            'categories' => CategoriesResource::collection($this['category']),
             'status' => StatusResource::make($this['status'])
         ];
     }

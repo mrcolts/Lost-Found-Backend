1af0c4f5508611607b828d84ef6aa1cf186c8a9b
MrColT <coltredstone@gmail.com>
2020-11-10 12:00:01 +0600

    1

--- app/Http/Controllers/FoundItemController.php
+++ app/Http/Controllers/FoundItemController.php
@@ -6,17 +6,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PostStoreRequest;
 use App\Http\Traits\AuthTrait;
+use Illuminate\Http\Request;
 
 class FoundItemController extends BaseController
 {
     use AuthTrait;
 
-    public function store(PostStoreRequest $request)
+    public function store(Request $request)
     {
         if($me = $this->takeUser())
         {
             return $me->full_name;
         }
+
+        $request->validate([
+            'title' => ['required', 'string'],
+            'description' => ['required', 'string'],
+            'category' => ['required', 'uuid', 'exists:categories,id'],
+            'image' => ['nullable', 'image', 'max:8192'],
+        ]);
 
         return 'anonymous user';
     }

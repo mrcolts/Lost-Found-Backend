1255c0c57ea5b024357bdabd4408fd81d7a899af
alexey <demchenko.alexey.s@gmail.com>
2020-11-10 10:59:48 +0600

    FoundItem

--- app/Http/Controllers/FoundItemController.php
+++ app/Http/Controllers/FoundItemController.php
@@ -4,18 +4,22 @@
 namespace App\Http\Controllers;
 
 
+use App\Http\Requests\PostStoreRequest;
 use App\Http\Traits\AuthTrait;
 
 class FoundItemController extends BaseController
 {
     use AuthTrait;
 
-    public function store()
+    public function store($request)
     {
         if($me = $this->takeUser())
         {
             return $me->full_name;
         }
+
+        $request = (new PostStoreRequest())->validate($request);
+
         return 'anonymous user';
 
     }

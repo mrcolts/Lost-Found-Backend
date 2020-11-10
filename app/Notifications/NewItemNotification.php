3a3bedee4487756c4d0ea05eecc5f092595fabb3
alexey <demchenko.alexey.s@gmail.com>
2020-11-10 06:38:01 +0600

    Sleep

--- app/Notifications/NewItemNotification.php
+++ app/Notifications/NewItemNotification.php
@@ -49,14 +49,14 @@ class NewItemNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        $url = config('common.web.lost_item_page') . "/$this->lost_item_id";
-        $body = '<br/> <img src="https://api.qrserver.com/v1/create-qr-code/?size=320x320&data=' . $url . '"/>';
+        $url = config('common.web.lost_item_page') . "/ " . $this->lost_item_id;
+        $body = '<br/> <img src="{{ https://api.qrserver.com/v1/create-qr-code/?size=320x320&data=' . $url . ' }}"/>';
         return (new MailMessage)
                 ->subject('Вы добавили новую предмет!')
                 ->greeting('Здравствуйте, $this->full_name!')
-            ->line("Предмет « $this->lost_item_name » был добавлен в ваш каинет.")
-            ->line($body)
-            ->line('С уважением, команда IT Kama Sutra');
+                ->line("Предмет « $this->lost_item_name » был добавлен в ваш каинет.")
+                ->line($body)
+                ->line('С уважением, команда IT Kama Sutra');
     }
 
     /**

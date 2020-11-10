126c65071c9fefbd0ced01e542473fcf3931dcc2
MrColT <coltredstone@gmail.com>
2020-11-10 09:37:14 +0600

    1

--- app/Notifications/NewItemNotification.php
+++ app/Notifications/NewItemNotification.php
@@ -49,14 +49,19 @@ class NewItemNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        $url = config('common.web.lost_item_page') . "/" . $this->lost_item_id;
-        $body = '<body><br/> <img src="{{ https://api.qrserver.com/v1/create-qr-code/?size=320x320&data=' . $url . ' }}"/></body>';
+        $url = 'https://api.qrserver.com/v1/create-qr-code/?size=320x320&data=' .
+            config('common.web.lost_item_page') .
+            '/' .
+            $this->lost_item_id;
         return (new MailMessage)
                 ->subject('Вы добавили новую предмет!')
-                ->greeting('Здравствуйте, $this->full_name!')
-                ->line("Предмет « $this->lost_item_name » был добавлен в ваш каинет.")
-                ->line($body)
-                ->line('С уважением, команда IT Kama Sutra');
+                ->greeting("Здравствуйте, $this->full_name!")
+                ->line("Собственность «$this->lost_item_name» была добавлена в ваш кабинет.")
+                ->line('Просим прикрепить QR-код находящийся ниже к своей собственности, чтобы в случае потери добрые люди смогли её вернуть 😊')
+                ->line('С уважением, команда IT Kama Sutra')
+                ->view('home.notifications.new_item', [
+                    'qr_url' => $url
+                ]);
     }
 
     /**
---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,248 @@
+{{--@component('mail::message')--}}
+{{-- Greeting --}}
+{{--@if (! empty($greeting))--}}
+{{--# {{ $greeting }}--}}
+{{--@else--}}
+{{--@if ($level === 'error')--}}
+{{--# @lang('Whoops!')--}}
+{{--@else--}}
+{{--# @lang('Hello!')--}}
+{{--@endif--}}
+{{--@endif--}}
+
+{{-- Intro Lines --}}
+{{--@foreach ($introLines as $line)--}}
+{{--{{ $line }}--}}
+
+{{--@endforeach--}}
+
+{{-- Action Button --}}
+{{--@isset($actionText)--}}
+{{--<?php--}}
+{{--    switch ($level) {--}}
+{{--        case 'success':--}}
+{{--        case 'error':--}}
+{{--            $color = $level;--}}
+{{--            break;--}}
+{{--        default:--}}
+{{--            $color = 'primary';--}}
+{{--    }--}}
+{{--?>--}}
+{{--@component('mail::button', ['url' => $actionUrl, 'color' => $color])--}}
+{{--{{ $actionText }}--}}
+{{--@endcomponent--}}
+{{--@endisset--}}
+
+{{-- Outro Lines --}}
+{{--@foreach ($outroLines as $line)--}}
+{{--{{ $line }}--}}
+
+{{--@endforeach--}}
+
+{{-- Salutation --}}
+{{--@if (! empty($salutation))--}}
+{{--{{ $salutation }}--}}
+{{--@else--}}
+{{--@lang('С уважением'),<br>--}}
+{{--{{ config('app.name') }}--}}
+{{--@endif--}}
+
+{{-- Subcopy --}}
+{{--@isset($actionText)--}}
+{{--@slot('subcopy')--}}
+{{--@lang(--}}
+{{--    "Если у вас возникли проблемы с нажатием кнопки \":actionText\",\n".--}}
+{{--    'скопируйте и вставьте нижеприведенный URL в ваш веб-браузер: ',--}}
+{{--    [--}}
+{{--        'actionText' => $actionText,--}}
+{{--    ]--}}
+{{--) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>--}}
+{{--@endslot--}}
+{{--@endisset--}}
+{{--@endcomponent--}}
+
+        <!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Movily</title>
+    <style>
+        /* RESPONSIVE AND MOBILE FRIENDLY STYLES */
+        @media only screen and (max-width: 620px) {
+            table[class=body] h1 {
+                font-size: 28px !important;
+                margin-bottom: 10px !important;
+            }
+
+            table[class=body] p,
+            table[class=body] ul,
+            table[class=body] ol,
+            table[class=body] td,
+            table[class=body] span,
+            table[class=body] a {
+                font-size: 16px !important;
+            }
+
+            table[class=body] .wrapper,
+            table[class=body] .article {
+                padding: 10px !important;
+            }
+
+            table[class=body] .content {
+                padding: 0 !important;
+            }
+
+            table[class=body] .container {
+                padding: 0 !important;
+                width: 100% !important;
+            }
+
+            table[class=body] .main {
+                border-left-width: 0 !important;
+                border-radius: 0 !important;
+                border-right-width: 0 !important;
+            }
+
+            table[class=body] .btn table {
+                width: 100% !important;
+            }
+
+            table[class=body] .btn a {
+                width: 100% !important;
+            }
+
+            table[class=body] .img-responsive {
+                height: auto !important;
+                max-width: 100% !important;
+                width: auto !important;
+            }
+        }
+
+        /* PRESERVE THESE STYLES IN THE HEAD */
+        @media all {
+            .ExternalClass {
+                width: 100%;
+            }
+
+            .ExternalClass,
+            .ExternalClass p,
+            .ExternalClass span,
+            .ExternalClass font,
+            .ExternalClass td,
+            .ExternalClass div {
+                line-height: 100%;
+            }
+
+            .apple-link a {
+                color: inherit !important;
+                font-family: inherit !important;
+                font-size: inherit !important;
+                font-weight: inherit !important;
+                line-height: inherit !important;
+                text-decoration: none !important;
+            }
+
+            #MessageViewBody a {
+                color: inherit;
+                text-decoration: none;
+                font-size: inherit;
+                font-family: inherit;
+                font-weight: inherit;
+                line-height: inherit;
+            }
+
+            .btn-primary table td:hover {
+                background-color: #34495e !important;
+            }
+
+            .btn-primary a:hover {
+                background-color: #34495e !important;
+                border-color: #34495e !important;
+            }
+        }
+    </style>
+</head>
+<body class=""
+      style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
+<table border="0" cellpadding="0" cellspacing="0" class="body"
+       style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;">
+    <tr>
+        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp;</td>
+        <td class="container"
+            style="font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; Margin: 0 auto; max-width: 580px; padding: 10px; width: 580px;">
+            <div class="content"
+                 style="box-sizing: border-box; display: block; Margin: 0 auto; max-width: 580px; padding: 10px;">
+
+                <!-- START CENTERED WHITE CONTAINER -->
+                {{--                <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">Email description</span>--}}
+                <table class="main"
+                       style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background: #ffffff;">
+
+                    <!-- START MAIN CONTENT AREA -->
+                    <tr>
+                        <!-- <div style="background-color: #F2F2F2;"> -->
+                        <img src="https://movily.app/assets/img/logo/mail-logo.jpg" style="width: 100%; height: 100%;"
+                             alt="Logo">
+                        <!-- </div> -->
+                        <td class="wrapper"
+                            style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding-left: 40px; padding-right: 40px; padding-top: 25px; padding-bottom: 50px;">
+                            <table border="0" cellpadding="0" cellspacing="0"
+                                   style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
+                                <tr>
+                                    <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
+                                        <p style="font-family: sans-serif; font-size: 20px; font-weight: bold; line-height: 30px; font-weight: normal; margin: 0; Margin-bottom: 15px;">{{ $greeting ?? '...' }}</p>
+                                        @foreach ($introLines as $line)
+                                            <p style="font-family: sans-serif; font-size: 16px; font-weight: normal; line-height: 26px; margin: 0; Margin-bottom: 15px;">
+                                                {{ $line }}
+                                            </p>
+                                        @endforeach
+
+                                        <table width="100%" cellspacing="0" cellpadding="0"
+                                               style="Margin-top: 30px; Margin-bottom: 30px;">
+                                            <tr>
+                                                <td>
+                                                    <table cellspacing="0" cellpadding="0">
+                                                        <tr>
+                                                            <td style="border-radius: 2px;" bgcolor="#eb314d">
+                                                                <a href="{{ $actionUrl  }}" target="_blank"
+                                                                   style="padding: 8px 12px; border: 1px solid #E65A55;border-radius: 2px;font-family: sans-serif;font-size: 14px; line-height: 20px; color: #ffffff;text-decoration: none;font-weight:bold;display: inline-block;">
+                                                                    {{ $actionText }}
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        @foreach ($outroLines as $line)
+                                            <p style="font-family: sans-serif; font-size: 16px; font-weight: normal; margin: 0; Margin-bottom: 15px; line-height: 26px;">
+                                                {{ $line }}
+                                            </p>
+                                        @endforeach
+                                        <hr style="color: #D0D6D9; background-color: #D0D6D9; border-width: 0; height: 2px; Margin-top: 30px; Margin-bottom: 30px;">
+                                        <p style="font-family: sans-serif; font-size: 12px; font-weight: normal; line-height: 18px; margin: 0; Margin-bottom: 15px;">
+                                            Если у вас возникли какие-либо проблемы с входом в систему, обратитесь в
+                                            службу поддержки:
+                                            <a href="mailto:support@movily.app">support@movily.app</a>
+                                        </p>
+                                        <hr style="color: #D0D6D9; background-color: #D0D6D9; border-width: 0; height: 2px; Margin-top: 30px; Margin-bottom: 30px;">
+                                        {{--                                        <a href="https://example.com" style="font-family: sans-serif; font-size: 12px; line-height: 18px; font-weight: normal; margin: 0; Margin-bottom: 15px;">Политика конфиденциальности</a>--}}
+
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <!-- END MAIN CONTENT AREA -->
+                </table>
+
+                <!-- END CENTERED WHITE CONTAINER -->
+            </div>
+        </td>
+        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp;</td>
+    </tr>
+</table>
+</body>
+</html>

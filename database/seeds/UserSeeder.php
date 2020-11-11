0ccb34d52542b84f5d9f3b53bb64b1d8be569021
demchenko.alexey.s@gmail.com <demchenko.alexey.s@gmail.com>
2020-11-11 10:28:05 +0600

    1

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,10 +15,25 @@ class UserSeeder extends Seeder
     {
         factory(User::class, 5)->create();
         /** @var User $user */
+
         User::create([
             'full_name' => 'Мистер Пользователь',
             'phone' => '+77010000000',
             'email'=>'user@mrcolt.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        User::create([
+            'full_name' => 'Шорин Алихан',
+            'phone' => '+77010000000',
+            'email'=>'skifcha09@yandex.kz',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        User::create([
+            'full_name' => 'Демченко Алексей',
+            'phone' => '+77010000000',
+            'email'=>'demchenko.alexey.s@gmail.com',
             'password' => Hash::make('12345678'),
         ]);
 
